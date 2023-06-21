80a5222c069b0c79316eff865b6f0f6629a68a83
Lynn Kim <lynn.kim@brandung.de>
2023-06-21 17:41:52 +0200

    optimize + css

--- src/Board.js
+++ src/Board.js
@@ -40,13 +40,20 @@ class Board extends Component {
       hasWon: false,
       board: this.createBoard(), // muss ich hier kein binding machen?/ func als props
     };
-    // TODO: set initial state
+    this.resetBtn = this.resetBtn.bind(this);
   }
 
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+  resetBtn() {
+    if (this.state.hasWon === true) {
+      this.setState({
+        hasWon: false,
+        board: this.createBoard(),
+      });
+    }
+  }
+
   createBoard() {
     let board = []; //weil hier row auch kommt, das Value wird hier x&y also zwei
-    // TODO: create array-of-arrays of true/false values
     // wichtig hier in func: nested loop- loop in loop
     for (let y = 0; y < this.props.nrows; y++) {
       let row = [];
@@ -58,7 +65,6 @@ class Board extends Component {
     return board;
   }
 
-  /** handle changing a cell: update board & determine if winner */
   flipCellsAround(coord) {
     console.log("FLIPPING!", coord);
     let { ncols, nrows } = this.props; // nochmal .this eindeutiger machen.
@@ -66,40 +72,35 @@ class Board extends Component {
     let [y, x] = coord.split("-").map(Number);
 
     function flipCell(y, x) {
-      // if this coord is actually on board, flip it
-
       if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
         board[y][x] = !board[y][x];
       }
     }
 
-    //Flip initial cell
     flipCell(y, x);
     flipCell(y, x - 1);
     flipCell(y, x + 1);
     flipCell(y - 1, x);
     flipCell(y + 1, x);
 
-    // TODO: flip this cell and the cells around it
-
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
     // let hasWon = false; //für hasWon ist das Wert benötigt //aber hier ist hardcoding, also ändere ich das
     let hasWon = board.every((row) => row.every((cell) => !cell)); //=false
     this.setState({ board: board, hasWon: hasWon });
   }
 
-  /** Render game board or winning message. */
-
   render() {
-    // if the game is won, just show a winning msg & render nothing else
     // warum hier: wenn der Benutzer Board hat, wird diese Message nicht angezeigt.
-    // TODO
     if (this.state.hasWon) {
-      return <h1>YOU WON!!!</h1>;
+      return (
+        <div className='Board-title'>
+          <div className='winner'>
+            <span className='neon-orange'>YOU</span>
+            <span className='neon-blue'>WIN!</span>
+          </div>
+        </div>
+      );
     }
-    // make table board
-    // TODO
+
     // nochmal nested array & loop
     let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++) {
@@ -119,13 +120,26 @@ class Board extends Component {
     }
     return (
       <div>
-        <div className='Board-title'>
-          <div className='neon-orange'>Lights</div>
-          <div className='neon-blue'>Out</div>
+        <div className='Game-container'>
+          <div className='Board-title'>
+            <div className='neon-orange'>Lights</div>
+            <div className='neon-blue'>Out</div>
+          </div>
+          <table className='Board'>
+            <tbody>{tblBoard}</tbody>
+          </table>
+          <button onClick={this.resetBtn}>new Game</button>
+          <div className='Intro-game'>
+            <h2>How to play</h2>
+            <p className='text'>
+              The game consists of a 5 by 5 grid of lights. When the game
+              starts, a random number or a stored pattern of these lights is
+              switched on. Pressing any of the lights will toggle it and the
+              adjacent lights. The goal of the puzzle is to switch all the
+              lights off, preferably with as few button presses as possible.
+            </p>
+          </div>
         </div>
-        <table className='Board'>
-          <tbody>{tblBoard}</tbody>
-        </table>
       </div>
     );
   }
